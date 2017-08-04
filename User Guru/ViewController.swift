cc864b5f367ef3ae904cdd2357fb438686929cfe
Ben Meyer <bmeyer@medsagetechnologies.com>
2017-08-03 20:54:21 -0400

    Initial Commit

---
+++ User Guru/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  User Guru
+//
+//  Created by Ben Meyer on 8/3/17.
+//  Copyright © 2017 bpmeyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
