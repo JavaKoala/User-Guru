456b0589e41d2930d12f2e9447b6a208144590dd
Ben Meyer <bmeyer@medsagetechnologies.com>
2017-08-12 11:11:05 -0400

    login - Added outlet for website text fields, actions for login and sign up.
    If the website is in the AppSettings then it will appear on view controller load
    
    If the website is not in the AppSettings the sign up button will add it

--- User Guru/ViewController.swift
+++ User Guru/ViewController.swift
@@ -7,12 +7,71 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var websiteTextField: UITextField!
+
+    @IBAction func loginButton(_ sender: Any) {
+        if let userWebsite = websiteTextField.text {
+            print (userWebsite)
+        }
+    }
+
+    @IBAction func signUpButton(_ sender: Any) {
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "AppSettings")
+        request.returnsObjectsAsFaults = false
+
+        // If the app settings exist update them
+        // If the app settings do not exist then create them
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                print(results)
+                // To Do: Update AppSettings
+            } else {
+                let appSettings = NSEntityDescription.insertNewObject(forEntityName: "AppSettings", into: context)
+                if let websiteURL = websiteTextField.text {
+                    appSettings.setValue(websiteURL, forKey: "website")
+                }
+            }
+        } catch {
+            // To Do: Handle Errors
+        }
+
+        do {
+            try context.save()
+        } catch {
+            // To Do: Handle Errors
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // If the website and email address are in the database, populate them
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "AppSettings")
+        request.returnsObjectsAsFaults = false
+
+        do {
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    if let websiteURL = result.value(forKey: "website") as? String {
+                        websiteTextField.text = websiteURL
+                    }
+                }
+            }
+        } catch {
+            // To Do: Handle Errors
+        }
     }
 
     override func didReceiveMemoryWarning() {
